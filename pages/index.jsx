1eb7c511bc7cdc9445fd9d6c07dc6ae234143c70
Officialibn1 <officialibn001@gmail.com>
2023-04-09 11:27:41 +0100

    Nextjs With Traversy

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,17 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Ninjas | Home</title>
-        <meta name="keywords" content="ninjas" />
-      </Head>
-      <h1 className={styles.title}>Homepage</h1>
-
-      <p className={styles.text}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, consectetur nemo. Ipsam excepturi officiis ratione repellendus pariatur, officia maxime voluptatibus, perspiciatis temporibus possimus sequi expedita, deserunt labore numquam ex explicabo?</p>
-
-      <p className={styles.text}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa veritatis esse qui harum adipisci ratione, quos non laboriosam ipsam fugit, animi, necessitatibus eligendi molestias aspernatur.</p>
-
-      <Link href='/ninjas' className={styles.btn}>See Ninjas Listing</Link>
+      <h1>Hello Next</h1>
    </>
   )
 }
